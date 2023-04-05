97ad3bbb8b99ad756a9f484ba64bf454e9b3103d
DaveLawper <129445996+DaveLawper@users.noreply.github.com>
2023-04-05 21:44:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 
 function transfert(){
-    const link = document.createElement("a");
     var json='{"Etat":1,"Satelite":0}';
     let data=new File([""],"data.json");
-    link.href = URL.createObjectURL(data);
-    link.download = "data.json";
-    link.click();
-    URL.revokeObjectURL(link.href);
+    //const link = document.createElement("a");
+    //link.href = URL.createObjectURL(data);
+    //link.download = "data.json";
+    //link.click();
+    // URL.revokeObjectURL(link.href);
     data=JSON.parse(json);
-    document.getElementById('data').innerHTML+=data.Etat,data.Satelite
+    document.getElementById('data').innerHTML=data.Etat+" "+data.Satelite
+    window.location.href = "server.php?data=" + json;
 };
